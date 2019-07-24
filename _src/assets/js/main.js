7e0cfc22b35f8e73fd80c74d0c083323293f0cfa
Maria Mata <matah.maria@gmail.com>
2019-07-25 00:51:54 +0200

    color section > added interative colapsable

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const collapsable = document.querySelector('.palette');
+const collapsableClick = document.querySelector('.collapsable');
+
+function openCollapsable() {
+
+  if (collapsable.classList.contains('container--open') === true) {
+    // Si está abierto (tiene la clase container--open), lo cierras
+    collapsable.classList.remove('container--open');
+  } else {
+    // si está cerrado (no tiene la clase), lo abres
+    collapsable.classList.add('container--open');
+  }
+
+}
+
+collapsableClick.addEventListener('click', openCollapsable);
