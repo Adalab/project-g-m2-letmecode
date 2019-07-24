a28808adceb465caeea02c980f2c22d05a0fcb56
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-24 23:06:33 +0200

    add accordion animation with js and fix class collapsable-visible

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//collapsable
+const form = document.querySelector('form');
+
+const onFormClick = (event) => {
+  const target = event.target;
+  //.closest es un querySelector hacia arriba desde el elemento en el que se ha hecho target.
+  const header = target.closest('.collapsable__header');
+
+  if (header) {
+    const nextBlock = header.nextElementSibling;
+
+    //falta añadir que la flecha se gire//
+    nextBlock.classList.toggle('collapsable--visible');
+  }
+
+
+}
+
+form.addEventListener('click', onFormClick)
