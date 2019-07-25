693b5cb85ea1ac3cadb7811404d626f93ba54d11
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-25 20:56:25 +0200

    clean code with functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,41 +3,50 @@
 //collapsable
 const form = document.querySelector('form');
 
+//functions
+function rotateIconFromHeader(header) {
+  const icon = header.querySelector('.fa-chevron-down');
+  icon.classList.toggle('collapsable--rotate');
+}
+
+function openCollapsable(header) {
+  rotateIconFromHeader(header);
+  //seleccionamos el elemento hermano de la cabecera (el contenido)
+  header.nextElementSibling.classList.toggle('collapsable--visible');
+}
+
+function closeCollapsable(block) {
+  rotateIconFromHeader(block.previousElementSibling);
+  block.classList.remove('collapsable--visible');
+}
+
+//callbacks
 const onFormClick = (event) => {
   const target = event.target;
   //.closest es un querySelector hacia arriba desde el elemento en el que se ha hecho target.
   const header = target.closest('.collapsable__header');
-
   //seleccionamos todos los elementos con clase collapsable--visible
   const openedBlocks = form.querySelectorAll('.collapsable--visible');
 
   //Si se ha hecho click en un elemento con la clase .collapsable__header(header)
   if (header) {
     //seleccionamos el elemento hermano de la cabecera (el contenido)
-  const nextBlock = header.nextElementSibling;
+    const nextBlock = header.nextElementSibling;
 
     //si hay elementos con la clase .collapsable--visible, collapsables abiertos
     if (openedBlocks.length) {
       //recorro el array que contiene todos los collapsables abiertos
       for (let i = 0; i < openedBlocks.length; i++) {
-        //si el "este" bloque no es el bloque asociado al header que he clickado
+        //si "este" bloque no es el bloque asociado al header que he clickado
         if (openedBlocks[i] !== nextBlock) {
-          openedBlocks[i].classList.remove('collapsable--visible');
-          openedBlocks[i].previousElementSibling.querySelector('.fa-chevron-down').classList.remove('collapsable--rotate');
-
+          closeCollapsable(openedBlocks[i]);
         }
       }
     }
 
-    nextBlock.classList.toggle('collapsable--visible');
-
-    //Rotate arrow icon//
-    const icon = header.querySelector('.fa-chevron-down');
-    icon.classList.toggle('collapsable--rotate');
-
+    openCollapsable(header);
   }
-
-
 }
 
-form.addEventListener('click', onFormClick)
+//listeners
+form.addEventListener('click', onFormClick);
