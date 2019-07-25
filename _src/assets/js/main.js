0b4dbe8aa39918b97a88dff3fe270d21a8f01460
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-25 20:28:38 +0200

    fix js collapsable behaviour

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 //collapsable
 const form = document.querySelector('form');
 
@@ -10,12 +8,32 @@ const onFormClick = (event) => {
   //.closest es un querySelector hacia arriba desde el elemento en el que se ha hecho target.
   const header = target.closest('.collapsable__header');
 
+  //seleccionamos todos los elementos con clase collapsable--visible
+  const openedBlocks = form.querySelectorAll('.collapsable--visible');
+
+  //Si se ha hecho click en un elemento con la clase .collapsable__header(header)
   if (header) {
-    const nextBlock = header.nextElementSibling;
+    //seleccionamos el elemento hermano de la cabecera (el contenido)
+  const nextBlock = header.nextElementSibling;
+
+    //si hay elementos con la clase .collapsable--visible, collapsables abiertos
+    if (openedBlocks.length) {
+      //recorro el array que contiene todos los collapsables abiertos
+      for (let i = 0; i < openedBlocks.length; i++) {
+        //si el "este" bloque no es el bloque asociado al header que he clickado
+        if (openedBlocks[i] !== nextBlock) {
+          openedBlocks[i].classList.remove('collapsable--visible');
+          openedBlocks[i].previousElementSibling.querySelector('.fa-chevron-down').classList.remove('collapsable--rotate');
+
+        }
+      }
+    }
 
     nextBlock.classList.toggle('collapsable--visible');
 
-  //falta añadir que la flecha se gire//
+    //Rotate arrow icon//
+    const icon = header.querySelector('.fa-chevron-down');
+    icon.classList.toggle('collapsable--rotate');
 
   }
 
