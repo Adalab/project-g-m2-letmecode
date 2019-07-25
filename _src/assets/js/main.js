207cede6c70361315e177902f5e8bfdc7123e30f
ClaBri <clabri@users.noreply.github.com>
2019-07-25 11:17:15 +0200

    form: name and job dinamic

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
+// Escribe la info del input en la tarjeta
+const formName = document.querySelector('.form__field-name');
+const previewName = document.querySelector('.preview-card__name');
+const formJob = document.querySelector('.form__field-job');
+const previewJob = document.querySelector('.preview-card__career');
 
-console.log('>> Ready :)');
+function writing(event) {
+    previewName.innerHTML = event.currentTarget.value;
+}
+formName.addEventListener('keyup', writing);
+
+function writing2(event) {
+    previewJob.innerHTML = event.currentTarget.value;
+}
+formJob.addEventListener('keyup', writing2);
