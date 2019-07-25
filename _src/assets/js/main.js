c761d80a008b6b1d53f871afee496062a466f735
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-25 11:46:33 +0200

    add first collapsable functional

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
+//declarar constantes
 const collapsable = document.querySelector('.palette');
 const collapsableClick = document.querySelector('.collapsable__header');
 
+//funciones
 function openCollapsable() {
 
-  if (collapsable.classList.contains('container--open') === true) {
-    // Si está abierto (tiene la clase container--open), lo cierras
-    collapsable.classList.remove('container--open');
-  } else {
-    // si está cerrado (no tiene la clase), lo abres
-    collapsable.classList.add('container--open');
-  }
-
+  collapsable.classList.toggle('container--close');
 }
 
+//
 collapsableClick.addEventListener('click', openCollapsable);
