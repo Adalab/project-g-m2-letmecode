3b3ff9f52113fcd3b35c8bb0e5bd37d147ef8b56
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-31 18:45:07 +0200

    add url-test to twitter message

--- _src/assets/js/_buttons.js
+++ _src/assets/js/_buttons.js
@@ -1,15 +1,17 @@
 'use strict';
 
+//añadir funcionalidad botón de twitter (compartir mensaje y url de la tarjeta creada)
 const submitButton = document.querySelector('.button-card');
 const twitterShareCard = document.querySelector('.form__success-twitter');
 const twitterButton = document.querySelector('.button-twitter');
 
 
-function twitter(){
+function twitter() {
   twitterShareCard.classList.remove('hidden');
   submitButton.classList.add('not-available');
-  const twitterText = 'Ey!%20This%20is%20my%20new%20profile%20card%20:)'
-  twitterButton.href = `https://twitter.com/intent/tweet?text=${twitterText}`;
+  const twitterText = 'Ey!%20This%20is%20my%20new%20profile%20card%20:)';
+  const cardLink = 'https://fonts.google.com/'; //link de prueba CAMBIAAAR!!
+  twitterButton.href = `https://twitter.com/intent/tweet?text=${twitterText} ${cardLink}`;
 }
 
 submitButton.addEventListener('click', twitter);
