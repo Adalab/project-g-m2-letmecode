a1bc06edae9ff08398e776ea838cfe0559133f57
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-07-31 15:43:49 +0200

    add pallete js partial

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -82,24 +82,6 @@ inputGitHub.addEventListener('keyup', onGitHubInput);
 
 
 
-//-----partial------------CODIGO PARA CAMBIAR DE COLORES EL PREVIEW DE USUARIO
 
-//escuchamos los input radio
-const inputRadio1 = document.querySelector('.js__radio1');
-const inputRadio2 = document.querySelector('.js__radio2');
-const inputRadio3 = document.querySelector('.js__radio3');
-
-const previewCard = document.querySelector('.js__preview-card');
-
-function changeColor (event) {
-        previewCard.classList.remove('preview-card-red');
-        previewCard.classList.remove('preview-card-green');
-        previewCard.classList.remove('preview-card-yellow');
-        previewCard.classList.add(event.currentTarget.value);
-}
-
-inputRadio1.addEventListener('click', changeColor);
-inputRadio2.addEventListener('click', changeColor);
-inputRadio3.addEventListener('click', changeColor);
 
 
