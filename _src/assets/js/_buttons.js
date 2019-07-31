029fd13ae2c8147a0c2ec5fa289e3aa0fc92e6af
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-31 18:39:53 +0200

    add tweet button with text to share

--- _src/assets/js/_buttons.js
+++ _src/assets/js/_buttons.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const submitButton = document.querySelector('.button-card');
-const twitterShare = document.querySelector('.form__success-twitter');
+const twitterShareCard = document.querySelector('.form__success-twitter');
+const twitterButton = document.querySelector('.button-twitter');
+
 
 function twitter(){
-  twitterShare.classList.remove('hidden');
+  twitterShareCard.classList.remove('hidden');
   submitButton.classList.add('not-available');
+  const twitterText = 'Ey!%20This%20is%20my%20new%20profile%20card%20:)'
+  twitterButton.href = `https://twitter.com/intent/tweet?text=${twitterText}`;
 }
 
 submitButton.addEventListener('click', twitter);
