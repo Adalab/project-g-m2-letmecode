7fb34851ff010fdcf78ca66b8cdad999a5a4e897
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-31 18:16:57 +0200

    add js create-card button to twitter share

--- _src/assets/js/_buttons.js
+++ _src/assets/js/_buttons.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const submitButton = document.querySelector('.button-card');
+const twitterShare = document.querySelector('.form__success-twitter');
+
+function twitter(){
+  twitterShare.classList.remove('hidden');
+  submitButton.classList.add('not-available');
+}
+
+submitButton.addEventListener('click', twitter);
+
+
+
