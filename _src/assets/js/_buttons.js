d578afcf51f6e1c0e859555276176e6d58a34d71
mariamata <matah.maria@gmail.com>
2019-08-02 11:56:06 +0200

    reset card preview

--- _src/assets/js/_buttons.js
+++ _src/assets/js/_buttons.js
@@ -4,6 +4,13 @@ const resetBtn = document.querySelector('.button__text');
 
 function resetea(){
    form.reset();
+   previewName.innerHTML = ('Nombre Apellidos');
+   previewJob.innerHTML = ('Front-end developer');
+   previewPhone.parentElement.classList.add('hidden');
+   previewEmail.parentElement.classList.add('hidden');
+   previewLinkedIn.parentElement.classList.add('hidden');
+   previewGitHub.parentElement.classList.add('hidden');
+
 }
 
 
