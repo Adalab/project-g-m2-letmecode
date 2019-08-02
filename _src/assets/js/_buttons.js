52729a625f5f89cbb81d52475ea35935946dbd88
mariamata <matah.maria@gmail.com>
2019-08-02 12:04:23 +0200

    reset colors and big image

--- _src/assets/js/_buttons.js
+++ _src/assets/js/_buttons.js
@@ -4,12 +4,16 @@ const resetBtn = document.querySelector('.button__text');
 
 function resetea(){
    form.reset();
+   previewCard.classList.remove('preview-card-red');
+   previewCard.classList.remove('preview-card-green');
+   previewCard.classList.remove('preview-card-yellow');
    previewName.innerHTML = ('Nombre Apellidos');
    previewJob.innerHTML = ('Front-end developer');
    previewPhone.parentElement.classList.add('hidden');
    previewEmail.parentElement.classList.add('hidden');
    previewLinkedIn.parentElement.classList.add('hidden');
    previewGitHub.parentElement.classList.add('hidden');
+   profileImage.style.backgroundImage = `url(${defaultImage})`;
 
 }
 
